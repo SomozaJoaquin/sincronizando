6c3594bc254c2bae915f0d7ee7597bb3fc09b862
GuadalupeAnzoategui <guadalupe.anzoategui@tecro.dev>
2023-03-15 17:10:33 -0300

    add h1 body

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <H1>Hola Joaco</H1>
+    
 </head>
 <body>
      <h2>Intentando romper todo</h2>
+     <H1>Hola Joaco</H1>
 </body>
 </html>
